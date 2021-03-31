1502c9b8bbc00807e0eaf2cafb614876b9a1b6a6
nanyi <2752467928@qq.com>
2021-03-31 11:09:04 +0800

    测试使用

---
+++ src/main/java/com/zb/demo/web/Test.java
@@ -0,0 +1,13 @@
+package com.zb.demo.web;
+
+/**
+ * @Author: Tsl
+ * @Description:
+ * @Date: Created in 11:06  2021/3/31
+ * @Modified By:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
